659c67aadb0818e65840d8a1d12458db3ba9a737
Sunny Pradhan <sun.pradh@gmail.com>
2026-04-23 17:17:39 +0200

    handling periodic boundary conditions while painting

--- qulgt/core/painting.py
+++ qulgt/core/painting.py
@@ -1,34 +1,70 @@
-# import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.transforms as transforms
-# from .trilattice import TriangularLattice
 
-def paint_lattice(lattice, transform: transforms.Transform | None = None):
+
+def paint_lattice(lattice,
+                  transform: transforms.Transform | None = None,
+                  show_links: bool = True,
+                  show_sites: bool = False
+                  ):
     sites = lattice.sites
     fig, ax = plt.subplots()
+
+    # Apply transformations if given
     if transform is None:
         tr = ax.transData
     else:
         tr = transform + ax.transData
+
+    # Draw sites
     xcoords = [ s[0] for s in sites ]
     ycoords = [ s[1] for s in sites ]
-    ax.scatter(xcoords, ycoords, zorder=10, transform=tr)
-    for s0 in sites:
-        ax.text(s0[0], s0[1]-0.15, f"({s0[0]}, {s0[1]})", transform=tr,
-                ha="center",
-                va="bottom",
-                backgroundcolor="#ffffff99",
-                color="blue",
-                zorder=15
-                )
-        for s1 in sites:
-            link = lattice.link(s0, s1)
-            if link:
-                ax.plot([s0[0], s1[0]], [s0[1], s1[1]], "k", transform=tr, zorder=1)
-                midpoint_x = (s0[0] + s1[0]) / 2
-                midpoint_y = (s0[1] + s1[1]) / 2
-                ax.text(midpoint_x, midpoint_y, f"{link}", transform=tr,
-                    va="center",
-                    backgroundcolor="#ffffff99",
-                    zorder=3
+    ax.scatter(xcoords, ycoords, zorder=15, transform=tr)
+
+    # Draw links
+    for k, site in enumerate(sites):
+        x0, y0 = site
+
+        if show_sites: # site labels
+            ax.text(x0, y0, f"{site}", transform=tr,
+                    ha="center",
+                    va="bottom",
+                    backgroundcolor="#ffffffcc",
+                    color="blue",
+                    zorder=10
+                    )
+
+        for next_site in sites[k+1:]:
+            x1, y1 = next_site
+            link = lattice.link(site, next_site)
+            if link is None:
+                continue
+            # Default case:
+            xs, ys = (x0, x1), (y0, y1)
+            linestyle = "-"
+
+            # Check boundary in case of PBC
+            # Boundary-crossing links are shown as dotted lines
+            if lattice.pbc_x or lattice.pbc_y:
+                for vec in lattice.lattice_vectors:
+                    ex, ey = vec
+                    # `next_site` connected to `site` by a lattice vector?
+                    if lattice.site_index((x1+ex, y1+ey)) == lattice.site_index(site):
+                        xs, ys = (x1, x1+ex), (y1, y1+ey)
+                        linestyle = ":"
+                    # `next_site` connected to `site` while crossing the boundary?
+                    if (lattice._mod_boundary((x0 + ex, y0 + ey)) != (x0 + ex, y0 + ey))\
+                        and (lattice.site_index((x0+ex, y0+ey)) == lattice.site_index(next_site)):
+                        xs, ys = (x1, x1-ex), (y1, y1-ey)
+                        linestyle = ":"
+
+            # Plot the links
+            ax.plot(xs, ys, "k", linestyle=linestyle, transform=tr, zorder=1)
+            if show_links:
+                text_x, text_y = (xs[0] + xs[1]) / 2, (ys[0] + ys[1]) / 2
+                ax.text(text_x, text_y, f"{link}", transform=tr,
+                        ha="center",
+                        va="center",
+                        backgroundcolor="#ffffffcc",
+                        zorder=3
                     )
